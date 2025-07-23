3009e588738dedf9807a7c1f97052d3680b485af
Pritesh Name <priteshmishra2125@gmail.com>
2025-07-23 17:34:46 +0530

    deploy url config 3

--- backend/utils/puppeteer.utils.js
+++ backend/utils/puppeteer.utils.js
@@ -4,6 +4,7 @@ export const launchBrowser = async () => {
   const browser = await puppeteer.launch({
     headless: "new",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    timeout: 60000,
   });
   const page = await browser.newPage();
   return { browser, page };
