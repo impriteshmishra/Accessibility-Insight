5359c5ee4c839b3c85a4ba9243cfcdb06326dc0d
Pritesh Name <priteshmishra2125@gmail.com>
2025-07-25 22:24:01 +0530

    puppeteer config

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL  ,   // "https://accessibility-insight.onrender.com/",      //  import.meta.env.VITE_BASE_URL   for local development
-    timeout:10000, //10s  when not responding 10s meanst server time out
+    // timeout:10000, //10s  when not responding 10s meanst server time out
     withCredentials:true
 })
 
